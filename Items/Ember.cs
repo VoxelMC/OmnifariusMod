3a87b489b01b0f3fd3d1e4eae919fe741703eeed
VoxelMC <voxelmc@hotmail.com>
2016-08-19 22:12:04 -0700

    Revert "Did some stupid stiff, reverting in a few"
    This reverts commit 19d8c94183f1bb3e3bbeee4c8e72ff457d9f0dd7.

---
+++ Items/Ember.cs
@@ -0,0 +1,37 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+/*namespace OmnifariusMod.Items
+{
+	public class Ember : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Ember";
+			item.damage = 40; //change
+			item.toolTip = ".."; //change
+			item.useStyle = 1;
+			item.useAnimation = 20; //change
+			item.useTime = 20; //change
+			item.useSound = 1;
+			item.autoReuse = true;
+			item.knockBack = 4f;
+			item.pick = 100; //change
+			item.melee = true;
+			item.scale = 1f;
+		}
+
+		public override void AddRecipe()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient();
+			recipe.AddTile();
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}*/

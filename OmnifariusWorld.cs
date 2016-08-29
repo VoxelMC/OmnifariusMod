546829dfc98afeeb0eb7f4ddbafcc1b7ee82cffc
GabeHasWon <gabriel@guevarameyer.com>
2016-08-28 20:40:06 -0500

    Updated ALL the code.
    Woah! That unicorn was MAGICAL.

--- OmnifariusWorld.cs
+++ OmnifariusWorld.cs
@@ -23,37 +23,38 @@ namespace OmnifariusMod
 
             tasks.Insert(genIndex + 1, new PassLegacy("The Inferno", delegate (GenerationProgress progress)
             {
-                progress.Message = "Fire-ifying the world\n\n\n";
+                progress.Message = "Creating Volcanoes\n\n\n[Gabe's Test Mod]";
                 progress.Set(0.7f);
-                int rand1 = Main.spawnTileX + (Main.rand.Next(220, 550));
+                int rand1 = Main.spawnTileX + (Main.rand.Next(350, 600));
                 int rand2 = Main.spawnTileY + (Main.rand.Next(-20, 50));
                 for (int i = 0; i < Main.rand.Next(70, 76); i++)
                 {
                     if (Main.tile[rand1, rand2].type != -1)
                     {
-                        WorldGen.TileRunner(rand1, rand2, Main.rand.Next(8, 10), Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), false, 0f, 0f, true, true);
+                        // WorldGen.TileRunner(rand1, rand2, Main.rand.Next(8, 10), Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
+                        WorldGen.TileRunner(rand1, rand2, 1.1, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), false, 0f, 0f, true, true);
                     }
                 }
                 for (int i = 0; i < 3; i++)
                 {
-                    MakeVolcano(rand1 + Main.rand.Next(-100, 100), rand2);
+                    MakeVolcano(rand1 + Main.rand.Next(0, 100), rand2);
                 }
-                /**/
-                for (int i = rand1; i < rand1 + 200; i++)
+                
+                for (int i = rand1 - 100; i < rand1 + 500; i++)
                 {
-                    for (int k = rand2; k < rand2 + 200; k++)
+                    for (int k = rand2; k < rand2 + 400; k++)
                     {
                         if (Main.tile[i, k].type != -1)
                         {
-                            if (Main.tile[i, k].type == 1)
-                            {
+                            if (Main.tile[i, k].type == 1 || Main.tile[i, k].type == 25 || Main.tile[i, k].type == 203)
+                            {//25 and 203 is corruption/crimson
                                 Main.tile[i, k].type = (ushort)mod.TileType("PetrifiedStoneTile");
                             }
                             if (Main.tile[i, k].type == 2)
                             {
                                 Main.tile[i, k].type = (ushort)mod.TileType("FlamingGrass");
-                            }
-                            if (Main.tile[i, k].type == 53 || Main.tile[i, k].type == 147)
+                            }//397 and 396 is to be removed 
+                            if (Main.tile[i, k].type == 53 || Main.tile[i, k].type == 147 || Main.tile[i, k].type == 397 || Main.tile[i, k].type == 396)
                             {
                                 Main.tile[i, k].type = (ushort)mod.TileType("BurntSand");
                             }
@@ -69,6 +70,7 @@ namespace OmnifariusMod
 
         public void MakeVolcano(int X, int Y)
         {// WorldGen.TileRunner(Main.spawnTileX + 376, Main.spawnTileY + -15, 6, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
+            ////digTunnel(float X, float Y, float xDir, float yDir, int Steps, int Size, bool Wet = false)
             WorldGen.TileRunner(X, Y - 10, 6, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
             WorldGen.TileRunner(X, Y - 5, 8, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
             WorldGen.TileRunner(X, Y, 10, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
@@ -79,8 +81,7 @@ namespace OmnifariusMod
             WorldGen.TileRunner(X, Y + 25, 44, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
             WorldGen.TileRunner(X, Y + 30, 52, Main.rand.Next(1, 3), mod.TileType("PetrifiedStoneTile"), true, 0f, 0f, true, true);
 
-            WorldGen.digTunnel(X, Y, 0, 10, 5, 2, false);
-
+            WorldGen.digTunnel(X, Y, 0, 0, 5, 20, false);
         }
     }
 }
